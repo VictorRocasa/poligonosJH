d49204159565bc96fbd7014e0ad412a9ffedb7bc
Victor <victorcapobianco01@yahoo.com.br>
2022-09-26 09:17:35 -0300

    'Descartado componente estoque'

--- src/main/webapp/app/entities/estoque/estoque.model.ts
+++
@@ -1,7 +0,0 @@
-import { IForma } from 'app/entities/forma/forma.model';
-import { IEstoquePoligonos } from './estoquePoligono.model';
-
-export interface IEstoque {
-  poligonos?: IEstoquePoligonos[] | null;
-  formas?: IForma[] | null;
-}
--- src/main/webapp/app/entities/estoque/estoquePoligono.model.ts
+++
@@ -1,7 +0,0 @@
-export interface IEstoquePoligonos {
-  lados: number | null;
-  tamanho: number | null;
-  ocorrencias: number | null;
-}
-
-export type NewPoligono = Omit<IEstoquePoligonos, 'id'> & { id: null };
